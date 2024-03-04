3bf3e9315f7b13e6dc27eba23fb77dcd8ce44c43
alexis-figueira <alexis.figueira@hotmail.com>
2024-03-04 17:27:02 -0300

    Algo de js

--- Estudios/JS/archivo.js
+++ Estudios/JS/archivo.js
@@ -1,4 +1,28 @@
-num = 4
-num2 = !num
- 
-alert(num2)    
+class Animal {
+    constructor (especie, edad, color){
+        this.especie = especie ;
+        this.edad = edad ;
+        this.color = color ;
+        // this.info = "Soy " + this.especie + ", tengo " + this.edad + " años y soy de color " + this.color ;
+        this.info = `Soy ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}` ;
+    }
+    verInfo(){
+        document.write(this.info + "<br>")
+    }
+}
+
+class Perro extends Animal {
+    constructor(especia, edad, color, raza){
+        super(especia, edad, color);
+        this.raza = raza ;
+    }
+}
+
+
+let perro = new Perro ("perro", 5, "marron", "doberman");
+let gato = new Animal ("gato", 2, "azul");
+let pajaro = new Animal ("perro", 5, "marron");
+
+perro.verInfo();
+gato.verInfo();
+pajaro.verInfo();
