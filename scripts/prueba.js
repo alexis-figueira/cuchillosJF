6fd55b6e98e77abb29bb46d04e70da063ab9d259
alexis-figueira <alexis.figueira@hotmail.com>
2024-03-11 17:49:05 -0300

    funciona el js en pruebas

--- scripts/prueba.js
+++ scripts/prueba.js
@@ -1,87 +1,71 @@
 /*hay que probar todo */
 
-let objeto =$.ajax({
+var arrProd = []
+
+$(document).ready(function(){
+    $.ajax({
         url: "../JSON/baseProd.json",
         type: "GET",
         dataType: "json"
     }).done(function(resultado){
-        console.log(resultado);
-        
-
-
-
-
+        // console.log("muestro resultado del AJAX")
+        // console.log(resultado)
+        arrProd = resultado;
+        // console.log("muestro var ARRPROD")
     }).fail(function(xhr, status, error){
         console.log(xhr); 
         console.log(status) ;
         console.log(error);
     })
+})
 
+$(".card-btn-ant").click(function(){
+    let idActive = 0 ;
+    for(let i=0 ; i< arrProd.length ; i++){
+        if($(".card-tit").text() == arrProd[i].nombre){
+            idActive = arrProd[i].id;
+            break
+        }
+    }
+    
+    if (idActive==0){
+        console.log("No se encontro el archivo")
+    } else if (idActive == 1){
+        $(".card-tit").text(arrProd[arrProd.length - 1].nombre)
+        $(".card-desc").text(arrProd[arrProd.length - 1].descripcion)
+        $(".card-lnk").attr("src", "." + arrProd[arrProd.length - 1].img)
+        $(".card-prc").text("$ " + arrProd[arrProd.length - 1].precio)
+    }else {
+        $(".card-tit").text(arrProd[idActive-2].nombre)
+        $(".card-desc").text(arrProd[idActive-2].descripcion)
+        $(".card-lnk").attr("src", "." + arrProd[idActive-2].img)
+        $(".card-prc").text("$ " + arrProd[idActive-2].precio)
+    }
 
+})
 
+$(".card-btn-sig").click(function(){
+    let idActive = 0 ;
+    for(let i=0 ; i< arrProd.length ; i++){
+        if($(".card-tit").text() == arrProd[i].nombre){
+            idActive = arrProd[i].id;
+            break
+        }
+    }
+    
+    if (idActive==0){
+        console.log("No se encontro el archivo")
+    } else if (idActive == arrProd.length){
+        $(".card-tit").text(arrProd[0].nombre)
+        $(".card-desc").text(arrProd[0].descripcion)
+        $(".card-lnk").attr("src", "." + arrProd[0].img)
+        $(".card-prc").text("$ " + arrProd[0].precio)
+    }else {
+        console.log(arrProd[idActive-1])
+        $(".card-tit").text(arrProd[idActive].nombre)
+        $(".card-desc").text(arrProd[idActive].descripcion)
+        $(".card-lnk").attr("src", "." + arrProd[idActive].img)
+        $(".card-prc").text("$ " + arrProd[idActive].precio)
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* {
-    "producto" : [
-        {
-            "id" :  1 ,
-            "nombre" : "Mi primer cuchillo",
-            "precio" : 3500,
-            "img": "./img/cuchillo alfombardo.jpg"
-        },
-        {
-            "id" :  2 ,
-            "nombre" : "Mi Segundo cuchillo",
-            "precio" : 2200,
-            "img": "./img/cuchillos.jpg"
-        },
-        {
-            "id" :  3 ,
-            "nombre" : "Mi tercero cuchillo",
-            "precio" : 1600,
-            "img": "./img/juego de picnic.jpg"
-        },
-        {
-            "id" :  4 ,
-            "nombre" : "Mi cuarto cuchillo",
-            "precio" : 1900,
-            "img": "./img/cuchNegro.jpg"
-        },
-        {
-            "id" :  5 ,
-            "nombre" : "Mi quinto cuchillo",
-            "precio" : 4500,
-            "img": "./img/cuchNegro.jpg"
-        },
-        {
-            "id" :  6 ,
-            "nombre" : "Mi sexto cuchillo",
-            "precio" : 5300,
-            "img": "./img/cuchNegro.jpg"
-        },
-        {
-            "id" :  7 ,
-            "nombre" : "Mi Septimo cuchillo",
-            "precio" : 6000,
-            "img": "./img/cuchNegro.jpg"
-        } 
-    ]
-}
-*/
+})
