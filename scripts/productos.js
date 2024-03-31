e1a6394167bc247da54c37feaf99858da43f42a2
alexis-figueira <71852240+alexis-figueira@users.noreply.github.com>
2024-03-31 19:00:04 -0300

    na

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,3 +1,4 @@
+// Productos por botones
 $(".item-cuch").click(function(){
     $(".img__card").remove();
     for (let i=0 ; i<arrProductos.cuchillos.length ; i++){
@@ -35,22 +36,60 @@ $(".item-bol").click(function(){
     $(".article-prod .img__card").css("animation-duration", "1.5s");
 });
 
-$("#buscar").click(function(){
-    let prodIngresado = $("#prodBuscado").val()
-    if(prodIngresado.toUpperCase() == "CUCHILLO" || prodIngresado.toUpperCase() == "CUCHILLOS" ){
-        alert("Indicaste " + prodIngresado);
-        $("#prodBuscado").val("Ingrese un producto");
-    }else if(prodIngresado.toUpperCase() == "MATE" || prodIngresado.toUpperCase() == "MATES" ){
-        alert("Indicaste " + prodIngresado);
-        $("#prodBuscado").val("Ingrese un producto");
-    }else if(prodIngresado.toUpperCase() == "BOMBILLA" || prodIngresado.toUpperCase() == "BOMBILLAS" ){
-        alert("Indicaste " + prodIngresado);
-        $("#prodBuscado").val("Ingrese un producto");
-    }else if(prodIngresado.toUpperCase() == "BOLEADORA" || prodIngresado.toUpperCase() == "BOLEADORAS" ){
-        alert("Indicaste " + prodIngresado);
-        $("#prodBuscado").val("Ingrese un producto");
-    }else {
-        alert("no se encontro ningun producto")
-        $("#prodBuscado").val("Ingrese un producto");
+// Productos por busqueda
+$("#buscar").click(fbp);
+
+$("#prodBuscado").keypress(function(e){
+    if (e.keyCode == 13){
+        fbp();
     }
-})
+});
+
+/// Funcion busqueda
+function fbp (){ 
+    let prodIngresado = $("#prodBuscado").val()
+    if (prodIngresado == null || prodIngresado == ""){
+        return;
+    };
+    if(prodIngresado.toUpperCase() == "CUCHILLO" || prodIngresado.toUpperCase() == "CUCHILLOS" ){
+        $("#prodBuscado").val("");
+        $(".img__card").remove();
+        for (let i=0 ; i<arrProductos.cuchillos.length ; i++){
+            setCard(arrProductos.cuchillos[i].nombre, arrProductos.cuchillos[i].descripcion, "../img/"+arrProductos.cuchillos[i].img, arrProductos.cuchillos[i].precio,"article-prod");
+        };
+        $(".article-prod .img__card").css("animation-name", "ani-translate-ing")
+        $(".article-prod .img__card").css("animation-duration", "1.5s");
+        $(".article-prod .img__card").css("z-index", "90");
+    }else if(prodIngresado.toUpperCase() == "MATE" || prodIngresado.toUpperCase() == "MATES" ){
+        $("#prodBuscado").val("");
+        $(".img__card").remove();
+        for (let i=0 ; i<arrProductos.mates.length ; i++){
+            setCard(arrProductos.mates[i].nombre, arrProductos.mates[i].descripcion, "../img/"+arrProductos.mates[i].img, arrProductos.mates[i].precio,"article-prod");
+        };
+        $(".article-prod .img__card").css("animation-name", "ani-translate-ing")
+        $(".article-prod .img__card").css("animation-duration", "1.5s");
+    }else if(prodIngresado.toUpperCase() == "BOMBILLA" || prodIngresado.toUpperCase() == "BOMBILLAS" ){
+        $("#prodBuscado").val("");
+        $(".img__card").remove();
+        for (let i=0 ; i<arrProductos.bombillas.length ; i++){
+            setCard(arrProductos.bombillas[i].nombre, arrProductos.bombillas[i].descripcion, "../img/"+arrProductos.bombillas[i].img, arrProductos.bombillas[i].precio,"article-prod");
+        };
+        $(".article-prod .img__card").css("animation-name", "ani-translate-ing")
+        $(".article-prod .img__card").css("animation-duration", "1.5s");
+    }else if(prodIngresado.toUpperCase() == "BOLEADORA" || prodIngresado.toUpperCase() == "BOLEADORAS" ){
+        $("#prodBuscado").val("");
+        $(".img__card").remove();
+        for (let i=0 ; i<arrProductos.boleadoras.length ; i++){
+            setCard(arrProductos.boleadoras[i].nombre, arrProductos.boleadoras[i].descripcion, "../img/"+arrProductos.boleadoras[i].img, arrProductos.boleadoras[i].precio,"article-prod");
+        };
+        $(".article-prod .img__card").css("animation-name", "ani-translate-ing")
+        $(".article-prod .img__card").css("animation-duration", "1.5s");
+    }else{
+        Swal.fire({
+            title: "No se encontro ningún producto",
+            text: "Intente de nuevo",
+            icon: "warning"
+          });
+        $("#prodBuscado").val("");
+    }
+};
