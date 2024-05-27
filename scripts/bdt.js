8555db88a2513c102350471cf4f717f08657c009
alexis-figueira <alexis.figueira@hotmail.com>
2024-05-27 17:15:39 -0300

    nada sale bien

--- scripts/bdt.js
+++ scripts/bdt.js
@@ -1,46 +1,36 @@
-// const MySQL = require('mysql');
+const mysql = require('mysql');
 
-// const conection = MySQL.createConnection ({
-// 	host:'localhost',
-// 	user:'root',
-// 	password:'',
-// 	database:'northwind'
-// });
+function respuesta (){
+    const conection = mysql.createConnection ({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'cuchillos_jf'
+    });
 
-// conection.connect( (err) => {
-// 	if(err) throw err ;
-// 	console.log ('la conexión funciona');
-// });
-// /*Nuevamente en terminal:
-// 	node .\mysql.js  --> mysql es el nombre del archivo js donde esto codifica
-// */
+    conection.connect( (err) => {
+        if(err) throw err ;
+        console.log ('la conexión funciona');
+    });
 
-// conection.query('SELECT * from categories', (err,rows) => {
-// 	if(err) throw err ;
-// 	console.log('Muestro toda la tabla:');
-// 	console.log(rows);
-// 	console.log("Muestro la cantidad de resultados:");
-// 	console.log(rows.length); 
-// 	console.log("Muestro el primer registro y lo trabajo como un objeto(desde el resultado):");
-// 	console.log(rows[0]);
-// 	console.log(rows[0].CategoryName);
-// 	console.log("Muestro el segundo registro y lo trabajo como un objeto(desde una variable)")
-// 	const usuarioUno = rows[1];
-// 	console.log(usuarioUno.CategoryID);
-// 	console.log(usuarioUno.CategoryName);
-// 	console.log(usuarioUno.Description);
-// });
+    const devolucion = conection.query('SELECT * from productos', (err,rows) => {
+            if(err) throw err ;
+            console.log ('los datos de la tabla son:');
+            console.log (rows);
+        });
 
-// /// Realizar un insert
-// const insertar = "INSERT INTO categories (CategoryId, CategoryName, Description) VALUES (NULL, 'Hamburguesitas o ke', 'Son altas burgas con cheddar')"
-// conection.query(insertar, (err,rows) => {
-// 	if (err) throw err ;
-// })
-
-// /// Realizar un delete
-// conection.query("DELETE FROM `categories` WHERE `CategoryID` = 9", (err, rows) =>{
-// })
-
-// conection.end()
+    conection.end();
+    return devolucion ;  
+}
 
 
+console.log("ahora muestro los datos en respuesta")
+console.log(respuesta())
+
+setTimeout(function(){
+    console.log("Hola Mundo");
+}, 100);
+console.log("setTimeout() Ejemplo...");
+
+
+console.log("acabo de mostrar")
